51e9258436928130b11728a2d6ff610d1b9b3866
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-12-06 01:10:47 +0400

    Current Value Subject

---
+++ CurrentValueSubject/CurrentValueSubject/Error/UserError.swift
@@ -0,0 +1,21 @@
+//
+//  UserError.swift
+//  CurrentValueSubject
+//
+//
+
+import Foundation
+
+enum UserError: Error {
+    case invalid
+}
+
+extension UserError {
+    
+    public var errorDescription: String {
+        switch self {
+        case .invalid:
+            return "Form is invalid"
+        }
+    }
+}
---
+++ CurrentValueSubject/CurrentValueSubject/Extension/UIViewController+Alert.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Alert.swift
+//  CurrentValueSubject
+//
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showFailed(message: String) {
+        
+        let alert = UIAlertController(title: "Failed",
+                                      message: message,
+                                      preferredStyle: .alert)
+        alert.addAction(.init(title: "Ok",
+                              style: .cancel))
+        self.present(alert,
+                     animated: true)
+    }
+}
