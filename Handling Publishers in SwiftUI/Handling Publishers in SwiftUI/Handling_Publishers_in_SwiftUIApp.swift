afe3eb4daa5a489b7d7a880caa2277ddd30006a2
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-12-06 01:19:19 +0400

    Handling Publishers in SwiftUI

---
+++ Handling Publishers in SwiftUI/Handling Publishers in SwiftUI/Handling_Publishers_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Handling_Publishers_in_SwiftUIApp.swift
+//  Handling Publishers in SwiftUI
+//
+//  Created by Shujaat-Hussain on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Handling_Publishers_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContactsView()
+        }
+    }
+}
---
+++ Handling Publishers in SwiftUI/Handling Publishers in SwiftUI/Views/ContactsView.swift
@@ -0,0 +1,59 @@
+//
+//  ContactsView.swift
+//  Handling Publishers in SwiftUI
+//
+//  Created by Shujaat-Hussain on 12/5/24.
+//
+
+import SwiftUI
+
+struct ContactsView: View {
+    
+    @State private var isPresented = false
+    @ObservedObject private var viewModel = ContactsViewModel()
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            VStack {
+                if(viewModel.contacts.isEmpty){
+                    Text("No Contacts")
+                        .font(.system(size: 16,
+                                      weight: .bold))
+                    Text("It seems that there are no contacts, why don't you add some above")
+                        .multilineTextAlignment(.center)
+                        .font(.system(size: 13,
+                                      weight: .regular))
+                        .padding(.horizontal, 16)
+                        .padding(.top, 4)
+                } else {
+                    ScrollView {
+                        LazyVGrid(columns: [GridItem(.flexible())], spacing: 8) {
+                            ForEach(viewModel.contacts) { contact in
+                                ContactView(contact: contact)
+                            }
+                        }
+                    }
+                }
+                
+            }
+            .navigationTitle("Contacts")
+            .navigationBarItems(trailing: Button(action: {
+                isPresented = true
+            }, label: {
+                Image(systemName: "plus")
+            }))
+            .sheet(isPresented: $isPresented,
+                   content: {
+                AddContactsView(viewModel: viewModel, isPresented: $isPresented)
+            })
+        }
+    }
+}
+
+struct ContactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContactsView()
+    }
+}
